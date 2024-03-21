1d8172d437df8724f8940441aa855132d7a40a71
quachthanhhung12349 <162932339+quachthanhhung12349@users.noreply.github.com>
2024-03-21 12:04:30 +0700

    test
    test

---
+++ BT1/Basic Data Types.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a; long long b; char c; float d; double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << '\n' << b << '\n' << c << '\n';
+    cout << setprecision(3) << fixed << d << '\n';
+    cout << setprecision(9) << fixed << e << '\n';
+    // Complete the code.
+    return 0;
+}
+
